90e8255c3c096e402d13bf21a7593f721db92fa9
Ben Morgan <morganbenjamin968@gmail.com>
2023-03-12 03:15:57 -0500

    Code for being able to read in Pattern and text into seperate variables.

--- src/kmpDFA/Main.java
+++ src/kmpDFA/Main.java
@@ -1,5 +1,29 @@
 package kmpDFA;
+import  java.io.*;
+import java.util.*;
 
 public class Main {
-
+	
+	public static void main(String[] args) throws Exception
+	{
+		File file = new File(
+				"C:\\Users\\glenm\\eclipse-workspace\\Knuth-Morris-Pratt\\test1.txt");
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		//read in the text from the first line
+		String text = br.readLine();
+		//read in the pattern from the second line
+		String pattern = br.readLine();
+		
+		System.out.println(text);
+		System.out.println(pattern);
+	}
+	
+	/*	
+	public static int[] buildDFAGraph(String pattern)
+	{
+		
+	}
+	*/
 }
